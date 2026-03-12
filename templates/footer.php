be9b668cedc089ea2edc107466a3a833832b492c
kanyanatThao <kanyanat21994@gmail.com>
2026-03-13 02:08:06 +0700

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,10 @@
-<footer>
-    <p>
-        &copy; <?= date('Y') ?>. All rights reserved by Mintkook^^.
-    </p>
+<footer class="bg-yellow-200 mt-10">
+    <div class="max-w-7xl mx-auto px-6 py-4 text-center">
+
+        <p class="text-yellow-900 text-sm">
+            &copy; <?= date('Y') ?>. All rights reserved by 
+            <span class="font-semibold">Mintkook^^</span>.
+        </p>
+
+    </div>
 </footer>
